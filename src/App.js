3ff4733ae27c462f19cfcdca99ebd2c92f72402d
StarkHK <kaushikhemant05@gmail.com>
2022-06-12 01:24:54 +0530

    Router links enabled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+	return (
+		<div>
+			<h1>This is a shop component</h1>
+		</div>
+	);
+};
 
 const App = () => {
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<Home />} />
+				<Route path='/' element={<Navigation />}>
+					<Route index element={<Home />} />
+					<Route path='shop' element={<Shop />} />
+				</Route>
 			</Routes>
 		</>
 	);
