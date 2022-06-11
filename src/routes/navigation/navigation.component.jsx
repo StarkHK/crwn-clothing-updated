1adfcf7fbc46d7e7bf5dd5402b695cd5367aca88
StarkHK <kaushikhemant05@gmail.com>
2022-06-12 01:40:20 +0530

    Nav bar changes

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { React, Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.style.scss';
 
 const Navigation = () => {
 	return (
 		<Fragment>
 			<div className='navigation'>
-				<Link className='nav-link' to='/'>
-					<div>Logo</div>
+				<Link className='logo-container' to='/'>
+					<CrwnLogo className='logo' />
 				</Link>
 				<div className='nav-links-container'>
 					<Link className='nav-link' to='/shop'>
